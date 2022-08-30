c7e9bb95eb12eafff1f87799d523d527525d16ca
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-08-30 23:34:28 +0200

    add database config & save user logic with encryption

--- src/handlers/sign_up_handler.rs
+++ src/handlers/sign_up_handler.rs
@@ -1,12 +1,14 @@
-use crate::models::{ConfirmationCode, UserSignUp};
-use lambda_http::http::StatusCode;
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+use bcrypt::{DEFAULT_COST, hash};
 use lambda_http::{Body, Error, IntoResponse, Request, RequestExt, Response};
+use lambda_http::http::StatusCode;
 use serde_json::json;
 
-// type E = Box<dyn std::error::Error + Sync + Send + 'static>;
+use crate::models::{ConfirmationCode, UserSignUp};
 
 pub async fn handle_sign_up(event: Request) -> Result<impl IntoResponse, Error> {
-    let _user_sign_up: UserSignUp = match event.payload() {
+    let user_sign_up: UserSignUp = match event.payload() {
         Ok(Some(user_sign_up)) => user_sign_up,
         Err(_err) => {
             return Ok(Response::builder()
@@ -26,19 +28,35 @@ pub async fn handle_sign_up(event: Request) -> Result<impl IntoResponse, Error> 
         }
     };
 
-    //TODO: Save user
+    let client = init_db().await;
 
-    //TODO: Generate confirmation code
+
+    //TODO: Check if user email unique
+
+    let _result = client.put_item()
+        .table_name("smartad")
+        .item("pk", AttributeValue::S(format!("user#{}", user_sign_up.email)))
+        .item("sk", AttributeValue::S("none".into()))
+        .item("email", AttributeValue::S(user_sign_up.email))
+        .item("password", AttributeValue::S(hash(user_sign_up.password, DEFAULT_COST).unwrap()))
+        .send()
+        .await;
+
+    //TODO: Generate confirmation code and save it
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .body(Body::from(
             json!(ConfirmationCode {
                 code: "123456".into()
-            })
-            .to_string(),
+            }).to_string(),
         ))
         .unwrap())
+}
+
+async fn init_db() -> Client {
+    let shared_config = aws_config::load_from_env().await;
+    Client::new(&shared_config)
 }
 
 #[cfg(test)]
@@ -46,9 +64,10 @@ mod tests {
     use lambda_http::http::StatusCode;
     use lambda_http::IntoResponse;
 
-    use super::super::utils::*;
     use crate::handlers::sign_up_handler::handle_sign_up;
     use crate::models::UserSignUp;
+
+    use super::super::utils::*;
 
     #[tokio::test]
     async fn should_return_ok_when_form_is_ok() {
@@ -57,7 +76,7 @@ mod tests {
                 email: "something".into(),
                 password: "pass".into(),
             })
-            .unwrap(),
+                .unwrap(),
         );
         let res = handle_sign_up(req).await.unwrap().into_response().await;
         assert_eq!(res.status(), StatusCode::OK);
